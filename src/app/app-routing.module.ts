0609aace21ca910ac50d3252ac9a574c993117ba
Bartek20015 <100288525+Bartek20015@users.noreply.github.com>
2022-03-10 19:31:48 +0100

    L1E3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutUsPageModule } from './pages/about-us.page-module';
 import { HomePageModule } from './pages/home.page-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ 
+        path: 'about-us', 
+        loadChildren: () => AboutUsPageModule
+      }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), HomePageModule, AboutUsPageModule],
