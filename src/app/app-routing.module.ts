96d4603d01fd277f3b98e4a16e9334b3f35e42a5
Bartek20015 <100288525+Bartek20015@users.noreply.github.com>
2022-03-10 18:51:07 +0100

    Drugi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsPageModule } from './pages/about-us.page-module';
 import { HomePageModule } from './pages/home.page-module';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), HomePageModule],
+  imports: [RouterModule.forRoot(routes), HomePageModule, AboutUsPageModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
