0c1b89c621d55239eec303c5d391f950f7d61dd3
Bartek20015 <100288525+Bartek20015@users.noreply.github.com>
2022-03-24 23:20:11 +0100

    L3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NavbarComponentModule } from '@navigation';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +9,7 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports: [ NavbarComponentModule,
     BrowserModule,
     AppRoutingModule
   ],
